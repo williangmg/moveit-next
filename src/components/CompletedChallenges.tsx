3ec80a7001be908efbbfde39200cae47dd1a8415
williangmg <willian_gattass@outlook.com>
2024-02-20 14:29:45 -0400

    Fim da terceira aula

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,10 +1,14 @@
-import styles from '../styles/components/CompletedChallenges.module.css'
+import { useContext } from "react";
+import styles from "../styles/components/CompletedChallenges.module.css";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 
-export function CompletedChallenges(){
-    return(
-        <div className={styles.completedChallengesContainer}>
-            <span>Desafio Completo</span>
-            <span>5</span>
-        </div>
-    );
+export function CompletedChallenges() {
+  const { challengesCompleted } = useContext(ChallengesContext);
+
+  return (
+    <div className={styles.completedChallengesContainer}>
+      <span>Desafio Completo</span>
+      <span>{challengesCompleted}</span>
+    </div>
+  );
 }
--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,16 +1,22 @@
+import { useContext } from 'react'
 import styles from '../styles/components/ExperienceBar.module.css'
+import { ChallengesContext } from '../contexts/ChallengesContext'
 
 export function ExperienceBar() {
+    const {currentExperience, experienceToNextLevel} = useContext(ChallengesContext)
+
+    const percentToNextLevel = (Math.round(currentExperience*100 )/ experienceToNextLevel)
+
     return (
         <header className={styles.experienceBar}>
             <span>0 XP</span>
             <div>
-                <div style={{ width: "50%" }} />
+                <div style={{ width: `${percentToNextLevel}%` }} />
                 <span className={styles.currentExperience} style={{ left: "50%" }}>
-                    300 XP
+                    {currentExperience}XP
                 </span>
             </div>
-            <span>680 XP</span>
+            <span>{experienceToNextLevel} XP</span>
         </header>
     )
 }
