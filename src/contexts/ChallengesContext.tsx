8f892b1cb4fe55b49dbc0a69eebb0a81e7010268
williangmg <willian_gattass@outlook.com>
2024-02-22 15:11:42 -0400

    Arrumei o level que não estava sobindo

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -64,17 +64,18 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     if (!activeChallenge) {
       return;
     }
+
     const { amount } = activeChallenge;
     let finalExperience = currentExperience + amount;
 
     if (finalExperience >= experienceToNextLevel) {
       finalExperience = finalExperience - experienceToNextLevel;
       levelUp();
+    }
 
       setCurrentExperience(finalExperience);
       setActiveChallenge(null);
       setChallengesCompleted(challengesCompleted + 1);
-    }
   }
 
   return (
